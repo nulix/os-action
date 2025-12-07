fe3988252c7a241db5663ace894d23799c0a09a1
Matija Tudan <matija.tudan@nulix.hr>
2025-12-07 12:42:08 +0100

    entrypoint.sh: remove unused fetch_os and deploy_ota_update functions
    Removed fetch_os and deploy_ota_update functions from entrypoint.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,62 +83,6 @@ build_os() {
   nulix build os
 }
 
-fetch_os() {
-  cd build/deploy/$MACHINE
-
-  if [ "$1" = "base" ]; then
-    NULIX_OS_VER="${BASE_NULIX_OS_VER}"
-    LOG_ACT_INF "Fetching base NULIX OS v$NULIX_OS_VER for $MACHINE"
-
-    if [ "$STEP_NAME" = "build-os" ]; then
-      wget https://files.nulix.io/$MACHINE/boot-artifacts-v2025.01.tar.gz
-      wget https://files.nulix.io/$MACHINE/kernel-artifacts-rpi-6.6.y.tar.gz
-    fi
-    wget https://files.nulix.io/$MACHINE/$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
-    wget https://files.nulix.io/$MACHINE/$OSTREE_REPO.tar.gz
-  elif [ "$1" = "user" ]; then
-    NULIX_OS_VER="${USER_NULIX_OS_VER}"
-    LOG_ACT_INF "Fetching user's NULIX OS v$NULIX_OS_VER for $MACHINE"
-
-    curl -f "https://api.nulix.io/ota/download?filename=$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz" \
-      -H "Authorization: Bearer $API_KEY_SECRET" \
-      -o $OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
-    curl -f "https://api.nulix.io/ota/download?filename=$OSTREE_REPO.tar.gz" \
-      -H "Authorization: Bearer $API_KEY_SECRET" \
-      -o $OSTREE_REPO.tar.gz
-  fi
-
-  echo "NULIX_OS_VER=${NULIX_OS_VER}" >> $GITHUB_ENV
-
-  tar xzf $OSTREE_REPO.tar.gz
-  mv -v $OSTREE_REPO ../../../rootfs
-  cp -v $OSTREE_ROOTFS-*.tar.gz ../../../rootfs
-  rm $OSTREE_REPO.tar.gz
-
-  cd ../../cache
-  curl -s -f "https://api.nulix.io/ota/download?filename=$UPD8_KEYS" \
-    -H "Authorization: Bearer $API_KEY_SECRET" \
-    -o $UPD8_KEYS || true
-  cd ../..
-}
-
-deploy_ota_update() {
-  LOG_ACT_INF "Deploying OTA update"
-
-  cd build/cache
-  curl -X POST "https://api.nulix.io/ota/upload?filename=$UPD8_KEYS" \
-    -H "Authorization: Bearer $API_KEY_SECRET" \
-    -F "file=@$UPD8_KEYS"
-
-  cd ../deploy/$MACHINE
-  curl -X POST "https://api.nulix.io/ota/upload?filename=$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz" \
-    -H "Authorization: Bearer $API_KEY_SECRET" \
-    -F "file=@$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz"
-  curl -X POST "https://api.nulix.io/ota/upload?filename=$OSTREE_REPO.tar.gz" \
-    -H "Authorization: Bearer $API_KEY_SECRET" \
-    -F "file=@$OSTREE_REPO.tar.gz"
-}
-
 LOG_ACT_DBG
 LOG_ACT_DBG "================================================"
 LOG_ACT_DBG "============ Action Input Variables ============"
@@ -159,25 +103,6 @@ case "$STEP_NAME" in
     fetch_apps
     build_os
     ;;
-  # deploy-os)
-  #   LOG_ACT_INF "Deploying NULIX OS"
-  #   ;;
-  # update-apps)
-  #   LOG_ACT_INF "Updating custom apps"
-  #   OSTREE_COMMIT_MSG="Updated custom compose apps"
-  #   init_nulix_build_env
-  #   fetch_os user
-  #   inject_apps
-  #   deploy_ota_update
-  #   ;;
-  # update-os)
-  #   LOG_ACT_INF "Updating NULIX OS"
-  #   OSTREE_COMMIT_MSG="Updated base NULIX OS and added custom compose apps"
-  #   init_nulix_build_env
-  #   fetch_os base
-  #   inject_apps
-  #   deploy_ota_update
-  #   ;;
   *)
     LOG_ACT_ERR "Unknown step: $STEP_NAME"
     return 1
