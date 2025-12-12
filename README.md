2f39fe9c491210e4386cf37cfd8063fe7fd5c36e
Matija Tudan <matija.tudan@nulix.hr>
2025-12-12 12:13:50 +0100

    action: add support for apps repo, custom SSH key, SSL cert and UPD8 keys
    - apps repo can be set from the input
    - SSH public key can be set as a variable
    - SSL cert can be set as a variable
    - UPD8 keys can be set as a secret
    - add new step to upload OS image to minio
    - add instructions for running locally using act
    
    Signed-off-by: Matija Tudan <matija.tudan@nulix.hr>

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Build NULIX OS with custom apps
 
-This GitHub Action builds NULIX OS with custom apps, uploads the image as an Actions artifact, and deploys OSTree repo to NULIX API.
+This GitHub Action builds NULIX OS with custom apps and uploads the image as an Actions artifact.
+
+Debug it locally using `act`:
+
+```bash
+act --container-architecture linux/arm64 \
+  -P ubuntu-24.04-arm=-self-hosted \
+  --input MACHINE=rpi3 \
+  --input DISTRO=alpine-none \
+  --input APPS_REPO=https://github.com/nulix/apps.git \
+  --input S3_BUCKET=user-id-1-project-id-2 \
+  --input S3_ENDPOINT=http://minio:9000 \
+  --input JOB_ID=1 \
+  -s UPD8_KEYS=test123 \
+  -s MACHINE_REG_TOKEN=123test \
+  -s AWS_ACCESS_KEY_ID=minio \
+  -s AWS_SECRET_ACCESS_KEY=minio123 \
+  --var SSH_PUBLIC_KEY=ssh-dummy-key \
+  --var SSL_CERT=ssl-dummy-cert
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,9 @@ MACHINE="$INPUT_MACHINE"
 DISTRO="$INPUT_DISTRO"
 APPS_REPO="$INPUT_APPS_REPO"
 COMPOSE_FILE="$INPUT_COMPOSE_FILE"
+S3_BUCKET="$INPUT_S3_BUCKET"
+S3_ENDPOINT="$INPUT_S3_ENDPOINT"
+JOB_ID="$INPUT_JOB_ID"
 
 LOG_ACT_ERR() {
   echo -e "${RED}[nulix/os-action]:${NC} ${@}"
@@ -33,14 +36,13 @@ LOG_ACT_DBG() {
 init_nulix_build_env() {
   LOG_ACT_INF "Initializing NULIX OS build environment"
 
-  if [ -z "$API_KEY_SECRET" ]; then
-    LOG_ACT_ERR "API_KEY secret is not set!"
+  if [ -z "$MACHINE_REG_TOKEN" ]; then
+    LOG_ACT_ERR "MACHINE_REG_TOKEN secret is not set!"
     exit 1
   fi
 
-  if [ -z "$MACHINE_REG_TOKEN_SECRET" ]; then
-    LOG_ACT_ERR "MACHINE_REG_TOKEN secret is not set!"
-    exit 1
+  if [ -z "$UPD8_KEYS" ]; then
+    LOG_ACT_WRN "UPD8_KEYS secret is not set!"
   fi
 
   source /nulix-os-venv/bin/activate
@@ -86,15 +88,37 @@ build_os() {
   nulix build os
 }
 
+upload_os() {
+  LOG_ACT_INF "Uploading OS image to MinIO for $MACHINE"
+
+  source /nulix-os-venv/bin/activate
+
+  FILES=$(ls nulix-os/build/deploy/${MACHINE}/nulix-os-*.img.bz2 2>/dev/null || true)
+  if [ -z "$FILES" ]; then
+    LOG_ACT_ERR "No OS image found to upload!"
+    exit 1
+  fi
+
+  for f in $FILES; do
+    LOG_ACT_DBG "Uploading $f..."
+    aws --endpoint-url ${S3_ENDPOINT} s3 cp "$f" s3://${S3_BUCKET}/job-${JOB_ID}/
+  done
+}
+
 LOG_ACT_DBG
 LOG_ACT_DBG "================================================"
 LOG_ACT_DBG "============ Action Input Variables ============"
 LOG_ACT_DBG "================================================"
-LOG_ACT_DBG "STEP_NAME:         $STEP_NAME"
-LOG_ACT_DBG "MACHINE:           $MACHINE"
-LOG_ACT_DBG "DISTRO:            $DISTRO"
-LOG_ACT_DBG "APPS_REPO:         $APPS_REPO"
-LOG_ACT_DBG "COMPOSE_FILE:      $COMPOSE_FILE"
+LOG_ACT_DBG "STEP_NAME:      $STEP_NAME"
+LOG_ACT_DBG "MACHINE:        $MACHINE"
+LOG_ACT_DBG "DISTRO:         $DISTRO"
+LOG_ACT_DBG "APPS_REPO:      $APPS_REPO"
+LOG_ACT_DBG "COMPOSE_FILE:   $COMPOSE_FILE"
+LOG_ACT_DBG "SSH_PUBLIC_KEY: $SSH_PUBLIC_KEY"
+LOG_ACT_DBG "SSL_CERT:       $SSL_CERT"
+LOG_ACT_DBG "S3_BUCKET:      $S3_BUCKET"
+LOG_ACT_DBG "S3_ENDPOINT:    $S3_ENDPOINT"
+LOG_ACT_DBG "JOB_ID:         $JOB_ID"
 LOG_ACT_DBG "================================================"
 LOG_ACT_DBG
 
@@ -105,6 +129,10 @@ case "$STEP_NAME" in
     init_nulix_build_env
     fetch_apps
     build_os
+    ;;
+  upload-os)
+    LOG_ACT_INF "Uploading OS image to MinIO"
+    upload_os
     ;;
   *)
     LOG_ACT_ERR "Unknown step: $STEP_NAME"
